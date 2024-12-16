06f4b2e6f56b0ec4d1976caf59cd5c2a0190f9fe
tyfnnn <tayfun.i@icloud.com>
2024-12-16 13:23:36 +0100

    Initial Commit

---
+++ CashFlowTeam/CashFlowTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  CashFlowTeamApp.swift
+//  CashFlowTeam
+//
+//  Created by Tayfun Ilker on 16.12.24.
+//
+
+import SwiftUI
+
+@main
+struct CashFlowTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
