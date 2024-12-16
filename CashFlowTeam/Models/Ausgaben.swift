8c475a35be7591b974100d4ea064fa5ed5827920
DilaraOeztas <bodurdilara54@gmail.com>
2024-12-16 15:32:04 +0100

    test

---
+++ CashFlowTeam/Models/Ausgaben.swift
@@ -0,0 +1,8 @@
+//
+//  Ausgaben.swift
+//  CashFlowTeam
+//
+//  Created by Dilara Öztas on 16.12.24.
+//
+
+import Foundation
---
+++ CashFlowTeam/Models/Budget.swift
@@ -0,0 +1,8 @@
+//
+//  Budget.swift
+//  CashFlowTeam
+//
+//  Created by Dilara Öztas on 16.12.24.
+//
+
+import Foundation
